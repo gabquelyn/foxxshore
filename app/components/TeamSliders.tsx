0b2c1c25dd0f6f9b514af67d8e73cf307283baa1
Arebamen <99688529+gabquelyn@users.noreply.github.com>
2024-01-02 19:51:49 +0100

    Update TeamSliders.tsx

--- app/components/TeamSliders.tsx
+++ app/components/TeamSliders.tsx
@@ -26,8 +26,6 @@ export default function TeamSlider() {
   var settings = {
     dots: true,
     infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
